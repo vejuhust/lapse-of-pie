dee1dfb79ed06ff1958b60ec0671581de7bd74dd
vejuhust <vejuhust@gmail.com>
2014-09-03 23:15:28 +0800

    implement adding watermark in convert_frame.sh

--- Lapse/convert_frame.sh
+++ Lapse/convert_frame.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-cp -v "$1" "$2"
+filelogo=logo.png
+filesrc="$1"
+filedest="$2"
+
+# apt-get install imagemagick
+composite -dissolve 75% -gravity northwest -geometry +50+50 \( "$filelogo" -thumbnail 50% \) "$filesrc" "$filedest"
