ca1d8a030b0ac7bba2da07b93354707c80f1b7b7
vejuhust <vejuhust@gmail.com>
2014-09-12 01:31:08 +0800

    Filter out photos by time range and file name

--- Lapse/filter_file.sh
+++ Lapse/filter_file.sh
@@ -7,4 +7,18 @@ offset="$3"
 index_head=`expr $limit + $offset`
 index_tail=`expr $limit`
 
-ls "$dirsrc"*.jpg | sort -r | head -n "$index_head" | tail -n "$index_tail" | sort
+hour_min=0
+hour_max=24
+
+for filename in $(ls "$dirsrc"*.jpg | sort -r)
+do
+    name=${filename##*snap}
+    datehour=$(echo "$name" | cut -d '-' -f 3 | cut -d '_' -f 1)
+    datemin=$(echo "$name" | cut -d '-' -f 3 | cut -d '_' -f 2)
+    if [ "$datehour" -ge "$hour_min" -a "$datehour" -lt "$hour_max" ];
+    then
+        raw_list+=($filename)
+    fi
+done
+
+printf -- '%s\n' "${raw_list[@]}" | head -n "$index_head" | tail -n "$index_tail" | sort
