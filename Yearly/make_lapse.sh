3a60f670ab1b736f3c2e674effa020b16487095b
Wei Ye <vejuhust@gmail.com>
2015-08-22 19:38:07 +0800

    Clean up scripts in Yearly/

--- Yearly/make_lapse.sh
+++ Yearly/make_lapse.sh
@@ -1,32 +1,11 @@
 #!/bin/bash
 
-if [ $# -ge 1 ];
-then
-    model="$1"
-else
-    model=c
-fi
+fps=30
+crf=18
+width=1296
+song=p2112104.mp4
+#convert=cp
 
-case "$model" in
-c )
-    day_offset=0
-    day_limit=700
-    hour_min=0
-    hour_max=23
-    fps=30
-    crf=18
-    width=1296
-    song=p2112104.mp4
-    #convert=cp
-    ;;
-*)
-    echo "invalid model"
-    exit 2
-    ;;
-esac
-
-offset=$(expr '(' $hour_max - $hour_min ')' \* $day_offset \* 60)
-limit=$(expr '(' $hour_max - $hour_min ')' \* $day_limit \* 60)
 dirsrc=/root/fy16/selected/
 dirdest=/root/fy16/result/
 dirtmp=/tmp/fy16lapseframetmp"$model"/
@@ -42,7 +21,7 @@ mkdir "$dirtmp"
 
 # prepare photos
 count=0
-for filesrc in $(./filter_file.sh "$dirsrc" "$limit" "$offset" "$hour_min" "$hour_max")
+for filesrc in $(ls "$dirsrc"*.jpg | sort -r)
 do
     count=`expr $count + 1`
     filedest=`printf ""$dirtmp"snap_%.4d.jpg" "$count"`
@@ -52,8 +31,6 @@ done
 # generate h264 version video for modern browsers' users
 ffmpeg -y -vn -f image2 -i "$dirtmp"snap_%04d.jpg -i "$bgmmp4" -shortest -r "$fps" -vf scale="$width":-1 -vcodec libx264 -crf "$crf" -tune stillimage -profile:v high -level 4.2 -acodec aac -ab 128k -strict experimental "$filemp4"
 
-# archive it
+# archive & share it
 mv -v "$filemp4" "$arcfile"
-
-# share it
 cp -v "$arcfile" "$dirpublic"
